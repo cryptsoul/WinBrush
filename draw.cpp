88c1e6e06978116a8389e5184f7c253fd6a6c24a
cryptsoul <lubomircvetkov4@gmail.com>
2025-07-21 18:57:41 +0300

    added menus

--- draw.cpp
+++ draw.cpp
@@ -1,18 +1,21 @@
-#ifdef UNICODE 
-#define UNICODE 
+#ifndef UNICODE
+#define UNICODE
 #endif
+#define FILE_MENU_NEW 1
+#define FILE_MENU_EXIT 2
 
 #include <windows.h>
 
 LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
 
-int WINAPI wWinMain (HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR pCmdLine, int nCmdShow){
+void AddMenus(HWND);
+HMENU hMenu;
 
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR pCmdLine, int nCmdShow) {
     const wchar_t CLASS_NAME[] = L"Paint windows class";
 
-    WNDCLASS wc = {};
-
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    WNDCLASSW wc = {};
+    wc.hCursor = LoadCursorW(NULL, L"IDC_ARROW");
     wc.hbrBackground = (HBRUSH)COLOR_BACKGROUND;
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInst;
@@ -23,17 +26,17 @@ int WINAPI wWinMain (HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR pCmdLine, int n
     }
 
     HWND hwnd = CreateWindowEx(
-        0, 
-        CLASS_NAME, 
-        L"Paint", 
+        0,
+        CLASS_NAME,
+        L"Paint",
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-        NULL, 
-        NULL, 
-        hInst, 
+        NULL,
+        NULL,
+        hInst,
         NULL
     );
-    
+
     if (hwnd == NULL)
     {
         return 0;
@@ -41,24 +44,58 @@ int WINAPI wWinMain (HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR pCmdLine, int n
 
     ShowWindow(hwnd, nCmdShow);
 
-    MSG msg = {};
+    MSG msg = { };
     while (GetMessage(&msg, NULL, 0, 0) > 0)
     {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-    
 
-    return 0; 
+    return 0;
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT umsg, WPARAM wp, LPARAM lp){
-
     switch (umsg)
     {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
+        case WM_COMMAND:
+
+            switch (wp)
+            {
+            case FILE_MENU_EXIT :
+            DestroyWindow(hwnd);
+                break;
+
+            case FILE_MENU_NEW :
+                MessageBeep(MB_ICONASTERISK); // MB_OK
+                break;
+            }
+
+        case WM_CREATE:
+            AddMenus(hwnd);
+            return 0;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
     }
     return DefWindowProc(hwnd, umsg, wp, lp);
+}
+
+void AddMenus(HWND hwnd ){
+    hMenu = CreateMenu();
+    HMENU hFileMenu = CreateMenu();
+    HMENU hSubMenu = CreateMenu();
+
+    AppendMenu (hSubMenu, MF_STRING, NULL, L"SubMenu Item");
+
+    AppendMenu (hFileMenu, MF_STRING, FILE_MENU_NEW, L"New");
+    AppendMenu(hFileMenu, MF_POPUP, (UINT_PTR)hSubMenu, L"Open SubMenu");
+    AppendMenu(hFileMenu, MF_SEPARATOR, NULL, NULL);
+    AppendMenu(hFileMenu, MF_STRING, FILE_MENU_EXIT, L"EXIT");
+
+    
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFileMenu, L"File");
+    AppendMenu(hMenu, MF_STRING, NULL, L"Help");
+ 
+
+    SetMenu(hwnd, hMenu);
 }
